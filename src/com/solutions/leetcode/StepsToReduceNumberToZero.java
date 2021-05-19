154ccab01bf4959089a8285a7aba8cffde0302c9
Pallavi-Prakash-01 <84026412+Pallavi-Prakash-01@users.noreply.github.com>
2021-05-19 13:07:12 +0530

    Number of steps required to reduce given number to zero

---
+++ src/com/solutions/leetcode/StepsToReduceNumberToZero.java
@@ -0,0 +1,27 @@
+package com.solutions.leetcode;
+
+import java.util.Scanner;
+
+public class StepsToReduceNumberToZero {
+	
+	 public int numberOfSteps(int num) {
+	        int count =0;
+	        while(num!=0){
+	            if(num%2==0)
+	                num=num/2;
+	            else
+	                num = num-1;
+	            count++;
+	        }
+	        return count;
+	    }
+	public static void main(String[] args) {
+		StepsToReduceNumberToZero srn = new StepsToReduceNumberToZero();
+		System.out.println("Enter a number : ");
+		Scanner input = new Scanner(System.in);
+		int num = input.nextInt();
+		System.out.println("Number of steps required to reduce entered number to zero are : "+srn.numberOfSteps(num));
+
+	}
+
+}
